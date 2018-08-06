25c298e70399bedfad695819488da494f3729b22
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2018-08-06 12:50:39 -0500

    ad

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,48 +9,48 @@
 var friend_Array = [
     {
       friendName: "Mickey Mouse",
-      photo: "http://via.placeholder.com/350x350/",
+      photo: "http://via.placeholder.com/350x150",
       scores: [5,5,5,5,5,5,5,5,5,5]
     },
     {
         friendName: "Goofy",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [4,4,4,4,4,4,4,4,4,4]
     },
     {
         friendName: "Donald Duck",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [3,3,3,3,3,3,3,3,3,3]
     },
     {
         friendName: "Moogly",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [1,1,1,1,1,1,1,1,1,1]
     },
     {
         friendName: "Snow White",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [2,2,2,2,2,2,2,2,2,2]
     }, 
     {
         friendName: "Cindarella",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [3,3,3,3,3,3,3,3,3,3]
     },
     {
         friendName: "Sleeping Beauty",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [4,4,4,4,4,4,4,4,4,4]
     },
     {
         friendName: "Cruella",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [1,1,1,1,1,1,1,1,1,1]
     },
 
     {
         friendName: "Daisy",
-        photo: "http://via.placeholder.com/350x350",
+        photo: "http://via.placeholder.com/350x150",
         scores: [5,5,5,5,5,5,5,5,5,5]
     }
 
